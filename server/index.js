96650296d81fa1412c7641b8386e93ca0fb79b46
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-06 13:54:34 -0700

    set up

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const os = require('os');
+const compression = require('compression');
+const bunyan = require('bunyan');
+
+const log = bunyan.createLogger({ name: 'production' });
+log.info('starting up');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(compression());
+
+app.use(express.static('public'));
+
+const server = app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  } else {
+    log.info(`listening at http://${os.networkInterfaces().lo0[0].address}:${port}`);
+  }
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  socket.emit('message', 'user connected');
+});
---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const App = () => (
+  <div>
+    <h1>whisperSHOUT</h1>
+  </div>
+);
+
+export default App;
