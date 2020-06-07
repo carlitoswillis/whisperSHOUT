a765cd42f11d467ac9614c16afe13e656c4f3a40
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-06 21:31:52 -0700

    add rooms

--- server/index.js
+++ server/index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const os = require('os');
 const compression = require('compression');
 const bunyan = require('bunyan');
-const bodyParser = require('body-parser');
-
-const log = bunyan.createLogger({ name: 'production' });
-log.info('starting up');
-
-const port = process.env.PORT || 3000;
 
 const app = express();
+const log = bunyan.createLogger({ name: 'production' });
+log.info('starting up');
+const port = process.env.PORT || 3000;
 
 app.use(compression());
-app.use(bodyParser());
-
+app.use(express.json());
 app.use(express.static('public'));
+
+app.post('/save/', (req, res) => {
+  // save here
+  const data = req.body;
+  res.end();
+});
 
 const server = app.listen(port, (err) => {
   if (err) {
@@ -27,11 +29,12 @@ const server = app.listen(port, (err) => {
 const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
-  // console.log(socket.client.jeader);
-  // console.log(socket.client.connection);
-  // console.log(socket.client.request._query);
-  socket.broadcast.emit('message', { username: 'system', outgoingMessage: `${socket.handshake.query.username} connected`, system: true });
+  socket.on('join', (data) => {
+    socket.join(data.room);
+    io.in(data.room).emit('message', { username: 'system', outgoingMessage: `${socket.handshake.query.username} connected to ${data.room}`, system: true });
+  });
+
   socket.on('message', (data) => {
-    io.emit('message', data);
+    io.in(data.room).emit('message', data);
   });
 });
